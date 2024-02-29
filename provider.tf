f4578d399e0b6637481e28c1b329f5baafbf8248
vinothvenky <98956763+vinothvenky@users.noreply.github.com>
2024-02-29 08:30:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,14 +14,11 @@ terraform {
 provider "aws" {
   region = "${var.AWS_REGION}"
   alias   = "pilot_provider"
-  access_key = "AKIAX7VJDOUYVHSBLAVG"
-  secret_key = "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"
-
+  
 }   
 
 provider "aws" {
   alias = "remotestate"
   region     = "us-east-1"
-  # access_key = "AKIAX7VJDOUYVHSBLAVG"
-  # secret_key = "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"
+  
 }
